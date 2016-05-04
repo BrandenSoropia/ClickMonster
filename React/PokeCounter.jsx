100eb4a6cdb7ef1c18f77893d41ebe0e184f9c3f
Branden Soropia <b.soropia@gmail.com>
2016-05-04 00:24:32 -0400

    Changed images src to downloaded pictures in Pictures directory.

--- React/PokeCounter.jsx
+++ React/PokeCounter.jsx
@@ -1,17 +1,24 @@
 function levelChecker(clicks) {
 	var pic;
-	if (clicks >= 15) {
+	if (clicks >= 20) {
+		// X evolve Charizard
+		pic = "Pictures/charizard-max.png";
+	} else if (clicks >= 15) {
 	  // Charizard
-	  pic = "http://cdn.bulbagarden.net/upload/thumb/7/7e/006Charizard.png/250px-006Charizard.png";
+	  // pic = "http://cdn.bulbagarden.net/upload/thumb/7/7e/006Charizard.png/250px-006Charizard.png";
+	  pic = "Pictures/Charizard.png";
 	} else if (clicks >= 10) {
 	  // Charmeleon
-	  pic = "http://cdn.bulbagarden.net/upload/thumb/4/4a/005Charmeleon.png/250px-005Charmeleon.png";
+	  // pic = "http://cdn.bulbagarden.net/upload/thumb/4/4a/005Charmeleon.png/250px-005Charmeleon.png";
+	  pic = "Pictures/Charmeleon.png";
 	} else if (clicks >= 5) {
 	  // Charmander
-	  pic = "http://orig13.deviantart.net/d44b/f/2011/120/8/6/pokemon___charmander_by_lilnutta10-d2vr4ov.jpg";
+	  // pic = "http://orig13.deviantart.net/d44b/f/2011/120/8/6/pokemon___charmander_by_lilnutta10-d2vr4ov.jpg";
+	  pic = "Pictures/charmander.png";
 	} else {
 	  // Pokeball
-	  pic = "http://vignette2.wikia.nocookie.net/toonmage/images/b/b7/Poke'ball.png/revision/latest?cb=20110306114739";
+	  // pic = "http://vignette2.wikia.nocookie.net/toonmage/images/b/b7/Poke'ball.png/revision/latest?cb=20110306114739";
+	  pic = "Pictures/pokeball.png";
 	}
 
 	return pic;
